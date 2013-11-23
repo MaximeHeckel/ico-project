f1158e69d44c299aff2cc62040ea93396478589d
Maxime Heckel <heckelmaxime@gmail.com>
2013-11-23 16:19:11 +0100

    Add some other type need details

--- project.ml
+++ project.ml
@@ -10,6 +10,15 @@ type expression =
   | Eadd of expression * expression
   | Emul of expression * expression
   | Ecomp of expression * expression
+  (*| Earr of expression[expression]*)
   | EAssign of string * expression;;
 
+type programme =
+  | Pvar of expression
+  | Pfun of fonction
+  | Pproc of procedure
 
+type instruction =
+  | Iaff of expression
+   ;;
+(* cf page 91 de la bible faut en discuter*)
