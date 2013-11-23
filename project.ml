1432f8cb7f663e27cd4296a02f34f03a9c1e02e1
Maxime Heckel <heckelmaxime@gmail.com>
2013-11-23 16:02:46 +0100

    Add type expression (not finished)

--- project.ml
+++ project.ml
@@ -3,3 +3,13 @@
 type 'a arbre = Vide | Feuille of 'a | Noeud of (
   'a arbre)*('a arbre);;
 
+type expression =
+  | Evar of int
+  | Evarb of bool
+  | Emoinun of int
+  | Eadd of expression * expression
+  | Emul of expression * expression
+  | Ecomp of expression * expression
+  | EAssign of string * expression;;
+
+
