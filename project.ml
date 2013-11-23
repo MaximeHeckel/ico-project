60a7205b4fb8708e860de9d04b2728731ecb23cc
Maxime Heckel <heckelmaxime@gmail.com>
2013-11-23 16:37:23 +0100

    Fixed type expression + add type comparaison and operateur

--- project.ml
+++ project.ml
@@ -3,20 +3,25 @@
 type 'a arbre = Vide | Feuille of 'a | Noeud of (
   'a arbre)*('a arbre);;
 
-type expression =
-  | Evar of int
-  | Evarb of bool
-  | Emoinun of int
-  | Eadd of expression * expression
-  | Emul of expression * expression
-  | Ecomp of expression * expression
-  (*| Earr of expression[expression]*)
-  | EAssign of string * expression;;
-
 type programme =
   | Pvar of expression
   | Pfun of fonction
-  | Pproc of procedure
+  | Pproc of procedure;;
+
+type expression =
+  | Const of int
+  | Var of string
+  | Bool of bool
+  | Moinun of int
+  | Add of operateur * expression * expression
+  | Mul of operateur * expression * expression
+  | Comp of comparaison * expression * expression
+  (*| Earr of expression[expression]*)
+  | Assign of string * expression;;
+
+type operateur =  Plus | Moins | Mult | Div;;
+type comparaison = Gt | Lt | Gte | Lte | Equals | NonEq;;
+
 
 type instruction =
   | Iaff of expression
