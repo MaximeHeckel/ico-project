67faee6cf24e878a43caaffd3e506c8152e48f88
MaximeHeckel <heckelmaxime@gmail.com>
2013-12-07 17:31:24 +0100

    Add binop to print.ml

--- print.ml
+++ print.ml
@@ -7,3 +7,15 @@ let rec typ() = function
   | TInt -> sprintf "integer"
   | TBool -> sprintf "boolean"
   | TArray -> sprintf "array of %a" typ t
+
+let binop = function
+  | OAdd -> sprintf "+"
+  | OSub -> sprintf "-"
+  | OMult -> sprintf "*"
+  | ODiv -> sprintf "/"
+  | OGe -> sprintf ">="
+  | OGt -> sprintf ">"
+  | OLe -> sprintf "<="
+  | OLt -> sprintf "<"
+  | OEq -> sprintf "="
+  | ONeq -> sprintf "<>"
