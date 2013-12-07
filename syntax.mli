0652334b76637b57ead8ad1fed559fca344e7a5c
MaximeHeckel <heckelmaxime@gmail.com>
2013-12-07 16:27:05 +0100

    Complete syntaxe.mli with EFunCall and procedure structure

--- syntax.mli
+++ syntax.mli
@@ -5,14 +5,17 @@ type expression =
   | EBinOp of expression * binop * expression
   | EArrayGet of expression * expression
   | EArrayAlloc of typ * expression
-
-(* Manque l'appel de fonction *)
+  | EFunCall of Primitive.calle * expression list
 
 and constant =
   | ConstBool of bool
   | ConstInt of int32
 
 (* and unop *)
+and typ =
+  | TInt
+  | TBool
+  | TArray of typ
 
 and condition =
   | CExpression of expression
@@ -21,13 +24,21 @@ and condition =
   | COr of condition * condition
 
 and instruction =
-  | IProcCall of call * expression list (* call par défaut je ne savais pas quoi mettre a voir*)
+  | IProcCall of Primitive.calle * expression list (* call par défaut je ne savais pas quoi mettre a voir*)
   | ISetVar of string * expression
   | ISetArray of expression * expression * expression (* Paramêtres : Tableau, index , éléments *)
   | IIf of condition * instruction * instruction
   | IWhile of condition * instruction
   | IBegin of expression list
+and procedure = {
 
+  arguments: string * typ list;
+
+  result: typ option;
+
+  body: instruction
+
+}
 and program = {
 
   (*Variables globales*)
