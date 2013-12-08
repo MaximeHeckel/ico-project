eb45f41c23f387e893c62f8f3505783d524bfc48
MaximeHeckel <heckelmaxime@gmail.com>
2013-12-08 14:50:20 +0100

    remove primitive.mli

--- primitive.mli
+++
@@ -1,9 +0,0 @@
-type primitive =
-  | Write (*Affichage entier*)
-  | Writeln (*Affichage entier + retour a la ligne*)
-  | Readln (*Lecture entier*)
-  | Alloc (*Allocation*)
-
-type calle =
-  | CUserFunction of string (*Procédure ou fonction définie par l'utilisateur*)
-  | CPrimitiveFunction of primitive (*Opération primitive*)
