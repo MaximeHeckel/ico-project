fd2c1702e53005d8ec10917d8b2e8dc019f499d2
MaximeHeckel <heckelmaxime@gmail.com>
2013-12-08 14:40:31 +0100

    new PPSyntax.mli

--- PPsyntax.mli
+++ PPsyntax.mli
@@ -1,55 +1,55 @@
-type expression =
-  | Econst of constant
-  | EGetVar of string
-  | EUnOp of unop * expression
-  | EBinOp of expression * binop * expression
-  | EArrayGet of expression * expression
-  | EArrayAlloc of typ * expression
-  | EFunCall of Primitive.calle * expression list
+type type_expr = Integer | Boolean | Array of type_expr;;
+type var_list = (string * type_expr) list;;
 
-and constant =
-  | ConstBool of bool
-  | ConstInt of int32
+type program = {
+    global_vars : var_list; 
+    (* variables globales *)
+    definitions : (string * definition) list;
+    (* fonctions et procédures globales *)
+    main : instruction;
+    (* corps principal du programme *)
+  } 
+and definition = {
+    arguments : var_list; result : type_expr option;
+    (* arguments  et type du résultat *)
+    local_vars : var_list;
+    (* variables locales *)
+    body : instruction;
+    (* corps de la fonction *)
+  }
 
-(* and unop *)
-and typ =
-  | TInt
-  | TBool
-  | TArray of typ
+and expression =
+  | Int of int | Bool of bool
+    (* constantes *)
+  | Bin of binop * expression * expression
+    (* expressions arithétiques *)
+  | Get of string
+    (* accès à une variable *)
+  | Function_call of string * expression list
+    (* appel de fonction *)
+  | Geti of expression * expression
+    (* accès dans un tableau à une position *)
+  | Alloc of expression * type_expr
+    (* Création d'un tableau d'une certaine taille *)
 
-and condition =
-  | CExpression of expression
-  | CNot of condition
-  | CAnd of condition * condition
-  | COr of condition * condition
+and binop =
+  | Plus | Minus | Times | Div
+    (* arithmétique *)
+  | Lt | Le | Gt | Ge | Eq | Ne 
+    (* comparaisons *) 
 
-and instruction =
-  | IProcCall of Primitive.calle * expression list
-  | ISetVar of string * expression
-  | ISetArray of expression * expression * expression (* Paramêtres : Tableau, index , éléments *)
-  | IIf of condition * instruction * instruction
-  | IWhile of condition * instruction
-  | IBegin of expression list
-and procedure = {
-
-  arguments: string * typ list;
-
-  result: typ option;
-
-  body: instruction
-
-}
-and program = {
-
-  (*Variables globales*)
-  globals: typ;
-
-  (*Définitions fonctions et procédures*)
-
-  defs: procedure;
-
-  (* Main *)
-
-  main: instruction
-
-}
+and instruction = 
+  | Set of string * expression
+    (* Affectation d'une variable *)
+  | Sequence of instruction list
+    (* Suite d'instructions *)
+  | If of expression * instruction * instruction
+  | While of expression * instruction
+  | Procedure_call of string * expression list
+    (* Appel de procédure *)
+  | Write_int of expression
+  | Writeln_int of expression
+    (* Ecriture d'un entier sans ou avec retour à la ligne *)
+  | Read_int of string
+    (* Affectation dans un tableau *)
+  | Seti of expression * expression * expression
