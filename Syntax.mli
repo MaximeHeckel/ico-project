8d097203eac11ba177bc52793af4bb7572ad8fc7
MaximeHeckel <heckelmaxime@gmail.com>
2013-12-16 01:03:41 +0100

    modified syntax.mli

--- Syntax.mli
+++ Syntax.mli
@@ -1,22 +1,11 @@
 type type_expr = Integer | Boolean | Array of type_expr;;
 type var_list = (string * type_expr) list;;
 
-type program = {
-    global_vars : var_list; 
-    (* variables globales *)
-    definitions : (string * definition) list;
-    (* fonctions et procédures globales *)
-    main : instruction;
-    (* corps principal du programme *)
-  } 
-and definition = {
-    arguments : var_list; result : type_expr option;
-    (* arguments  et type du résultat *)
-    local_vars : var_list;
-    (* variables locales *)
-    body : instruction;
-    (* corps de la fonction *)
-  }
+type program =Program of var_list * (func_def list) * instruction
+
+and func_def = Func_def of string * definition
+
+and definition = Definition of var_list * type_expr option * var_list * instruction
 
 and expression =
   | Int of int | Bool of bool
@@ -31,14 +20,20 @@ and expression =
     (* accès dans un tableau à une position *)
   | Alloc of expression * type_expr
     (* Création d'un tableau d'une certaine taille *)
+  | Read
+  | Readln
 
 and binop =
   | Plus | Minus | Times | Div
     (* arithmétique *)
-  | Lt | Le | Gt | Ge | Eq | Ne 
-    (* comparaisons *) 
+  | Or |  And
+  | Lt | Le | Gt | Ge | Eq | Ne
+    (* comparaisons *)
 
-and instruction = 
+and unop =
+  | UMinus | Not
+
+and instruction =
   | Set of string * expression
     (* Affectation d'une variable *)
   | Sequence of instruction list
@@ -47,9 +42,7 @@ and instruction =
   | While of expression * instruction
   | Procedure_call of string * expression list
     (* Appel de procédure *)
-  | Write_int of expression
-  | Writeln_int of expression
+  | Write of expression
+  | Writeln of expression
     (* Ecriture d'un entier sans ou avec retour à la ligne *)
-  | Read_int of string
-    (* Affectation dans un tableau *)
   | Seti of expression * expression * expression
