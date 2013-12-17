c04865ad66c56f3e209b020030fabb1f4a0d94eb
Maxime Heckel <heckelmaxime@gmail.com>
2013-12-17 22:11:16 +0100

    comment code

--- main.ml
+++ main.ml
@@ -6,12 +6,11 @@ let main () =
   let name = Sys.argv.(1) in
   let ic = open_in name in
     let lexbuf = Lexing.from_channel ic in
-    (* while true do *)
-      let pgm = Pseudopascal.input Lexer.token lexbuf in
-      let graph_appel = make_call_graph pgm in print_call_graph graph_appel name;
-      (*Printf.printf "Parsing OK\n" *); 
+    while true do
+      let parse = Pseudopascal.input Lexer.token lexbuf in
+      let graph = make_call_graph parse in print_call_graph graph name;
       exit 0;
-    (* done *)
+    done
   with End_of_file -> exit 0
 ;;
 
